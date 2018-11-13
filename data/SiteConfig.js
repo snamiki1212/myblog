ab1ecd67b0d62641bc01ac8b97290c0ebc0cbe3d
snamiki1212 <snamiki1212@arsaga.jp>
2018-11-13 12:30:22 +0900

    Update: gatsby config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteTitle: "EdgeTechBlog", // Site title.
+  siteTitle: "なみきブログ", // Site title.
   siteTitleShort: "先端技術を追いかけるエンジニアのブログ", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "EdgeTechBlog", // Alternative site title for SEO.
+  siteTitleAlt: "なみきブログ", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://snamiki1212", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
@@ -17,9 +17,9 @@ module.exports = {
   userName: "Shun Namiki", // Username to display in the author segment.
   userTwitter: "snamiki1212", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "Japan", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userAvatar: "/logos/logo-48.png", // User avatar to display in the author segment.
   userDescription:
-    "先端技術を追いかけるエンジニア\nElixir/Phoenix, PHP/Laravel", // User description to display in the author segment.
+    "先端技術を追いかけるエンジニアのブログ", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
@@ -36,7 +36,12 @@ module.exports = {
       label: "Email",
       url: "mailto:snamiki1212@gmail.com",
       iconClassName: "fa fa-envelope"
-    }
+    },
+    {
+      label: "Linkedin",
+      url: "https://www.linkedin.com/in/snamiki1212",
+      iconClassName: "fa fa-linkedin"
+    }    
   ],
   copyright: "Copyright © 2018. Shun Namiki" // Copyright string for the footer of the website and RSS feed.
 };
