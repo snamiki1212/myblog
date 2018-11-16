97c0cd2b6d59d219fbbd1e6a74e2ddc87a0c281b
snamiki1212 <snamiki1212@arsaga.jp>
2018-11-17 05:14:29 +0900

    Update: about me

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -19,7 +19,7 @@ module.exports = {
   userLocation: "Shibuya, Japan", // User location to display in the author segment.
   userAvatar: "/logos/logo_48.jpg", // User avatar to display in the author segment.
   userDescription:
-  "いまどき技術が好きなエンジニアによる技術と生存戦略を書く雑記ブログ。SIer4年/Web1年半でお堅い金融システムから最近のスマホゲーム開発までを担当して、バックエンドを中心にフロント/インフラを触りながらディレクター/ディベロッパー/チームリーダーなどをこなしてきた。言語はPHP/LaravelからElixir/Phoenixなど。仕事依頼やTwitterフォロー、IT系の相談など気軽にどうぞ。経歴の詳細はLinkedinを参照。", // User description to display in the author segment.
+  "いまどき技術が大好きなエンジニアによる技術と生存戦略の雑記ブログ。SIer4年/Web1年半でお堅い金融システムから最近のスマホゲーム開発までを担当、バックエンドを中心にフロント/インフラを触りながらエンジニア/ディレクター/チームリーダーなどをこなしてきた。言語はPHP/LaravelからElixir/Phoenixなど。Twitterフォローや仕事依頼や相談など気軽にどうぞ。更に詳しい経歴はLinkedinを参照。", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
