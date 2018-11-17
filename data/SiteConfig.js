92167457f6084f6298b89c3ab0a67541263133ff
snamiki1212 <snamiki1212@arsaga.jp>
2018-11-17 18:48:11 +0900

    Refactor about me

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -18,7 +18,7 @@ module.exports = {
   userTwitter: "snamiki1212", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "Shibuya, Japan", // User location to display in the author segment.
   userAvatar: "/logos/logo_48.jpg", // User avatar to display in the author segment.
-  userDescription: "いまどき技術が大好きなエンジニアによる技術と生存戦略の雑記ブログ。SIer4年/Web1年半でお堅い金融システムから最近のスマホゲーム開発までを担当し、PHP/LaravelやElixir/Phoenixなどのバックエンドを中心にフロント/インフラを触りながらエンジニア/ディレクター/チームリーダーなどをこなしてきた。更に詳しい経歴はLinkedinを参照。Twitterフォローや仕事依頼や相談など気軽にどうぞ。", // User description to display in the author segment.
+  userDescription: "いまどき技術が大好きなエンジニアによる技術と生存戦略などを書く雑記ブログ。SIer4年/Web1年半でお堅い金融システムから最近のスマホゲーム開発までを担当し、PHP/LaravelやElixir/Phoenixなどのバックエンドを中心にフロント/インフラを触りながらエンジニア/ディレクター/チームリーダーなどを経験。更に詳しい経歴はLinkedinを参照。Twitterフォローや仕事依頼や相談など気軽にどうぞ。", // User description to display in the author segment.
   userIntroduction: "Freelance full-stack Endigneer @ Shibuya, Japan", // short user introduction
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
