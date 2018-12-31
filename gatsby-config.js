2fba231fb2afbe7f1e7d24ebd9bf621d91e595c2
snamiki1212 <snamiki1212@gmail.com>
2018-12-31 22:37:29 +0900

    Add gatsby-remark-twitter library

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,9 +49,10 @@ module.exports = {
           {
             resolve: "gatsby-remark-responsive-iframe"
           },
+          "@weknow/gatsby-remark-twitter",
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers"
+          "gatsby-remark-autolink-headers",
         ]
       }
     },
