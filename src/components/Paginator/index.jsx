d38314a9f2368df16f294da67caa87fc02110eb7
snamiki1212 <snamiki1212@gmail.com>
2019-03-26 08:11:37 +0800

    Update paginator

---
+++ src/components/Paginator/index.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Link } from "gatsby";
+import "./Paginator.scss";
+
+class Paginator extends Component{
+  render(){
+
+    const { currentPage, numPages } = this.props.pageContext
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+    const nextPage = (currentPage + 1).toString()
+
+    return(
+      <div>
+        <ul
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            listStyle: 'none',
+            padding: 0,
+          }}
+        >
+          {!isFirst && (
+            <Link to={prevPage} rel="prev">
+              ← Previous Page
+            </Link>
+          )}
+          {Array.from({ length: numPages }, (_, i) => (
+            <li
+              key={`pagination-number${i + 1}`}
+              style={{
+                margin: 0,
+              }}
+            >
+              <Link
+                to={`/${i === 0 ? '' : i + 1}`}
+                style={{
+                  textDecoration: 'none',
+                  color: i + 1 === currentPage ? '#ffffff' : '',
+                  background: i + 1 === currentPage ? '#007acc' : '',
+                }}
+              >
+                {i + 1}
+              </Link>
+            </li>
+          ))}
+          {!isLast && (
+            <Link to={nextPage} rel="next">
+              Next Page →
+            </Link>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Paginator;
--- src/templates/index.jsx
+++ src/templates/index.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 import Layout from "../layout";
 import PostListing from "../components/PostListing";
 import HeaderTitle from "../components/HeaderTitle";
 import SEO from "../components/SEO";
+import Paginator from "../components/Paginator";
 import config from "../../data/SiteConfig";
 
 class Index extends React.Component {
   render() {
     const postEdges = this.props.data.allMarkdownRemark.edges;
-
-    
-    const { currentPage, numPages } = this.props.pageContext
-    const isFirst = currentPage === 1
-    const isLast = currentPage === numPages
-    const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
-    const nextPage = (currentPage + 1).toString()
 
     return (
       <Layout location={this.props.location} title={<HeaderTitle />}>
@@ -27,48 +21,7 @@ class Index extends React.Component {
           </Helmet>
           <SEO postEdges={postEdges} />
           <PostListing postEdges={postEdges} />
-        </div>
-        <div>
-          <ul
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              listStyle: 'none',
-              padding: 0,
-            }}
-          >
-            {!isFirst && (
-              <Link to={prevPage} rel="prev">
-                ← Previous Page
-              </Link>
-            )}
-            {Array.from({ length: numPages }, (_, i) => (
-              <li
-                key={`pagination-number${i + 1}`}
-                style={{
-                  margin: 0,
-                }}
-              >
-                <Link
-                  to={`/${i === 0 ? '' : i + 1}`}
-                  style={{
-                    textDecoration: 'none',
-                    color: i + 1 === currentPage ? '#ffffff' : '',
-                    background: i + 1 === currentPage ? '#007acc' : '',
-                  }}
-                >
-                  {i + 1}
-                </Link>
-              </li>
-            ))}
-            {!isLast && (
-              <Link to={nextPage} rel="next">
-                Next Page →
-              </Link>
-            )}
-          </ul>
+          <Paginator pageContext={this.props.pageContext}/>
         </div>
       </Layout>
     );
