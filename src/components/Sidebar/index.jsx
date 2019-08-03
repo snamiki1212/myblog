a0e4b7c2ac6bdc11db6d95071340ecfd3ef819bb
snamiki1212 <snamiki1212@gmail.com>
2019-08-04 00:35:57 +0900

    Refactor to update css

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
-import './Sidebar.scss';
+import styled from 'styled-components';
 import Author from '../Author';
+import {colors} from '../../../data/color';
 
 class Sidebar extends Component {
   render() {
     const {tags} = this.props;
+
+    const Wrapper = styled.div`
+      flex: 1;
+      width: 100%;
+      min-width: 250px;
+      padding: 24px;
+
+      background-color: ${colors['bg-white-2']};
+    `;
     return (
-      <div id="sidebar">
+      <Wrapper>
         <Author />
-      </div>
+      </Wrapper>
     );
   }
 }
