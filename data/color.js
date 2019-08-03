4effe1621ff6eba1286f085e340de94b4363f23f
snamiki1212 <snamiki1212@gmail.com>
2019-08-04 00:32:01 +0900

    Refactor to update css

---
+++ data/color.js
@@ -0,0 +1,26 @@
+export const colors = {
+  //
+  'bg-black-1': '#222222',
+  'bg-vivid-1': '#f0db00',
+  'bg-white-1': '#ffffff',
+  'bg-white-2': '#f3f3f3',
+  'bg-white-3': '#e5e3df',
+  'fc-black-1': '#222222',
+  'fc-black-2': '#111111',
+  'fc-vivid-1': '#f0db00',
+  'fc-vivid-2': '#e0cc00',
+  'fc-vivid-3': '#d0bb00',
+  'fc-white-1': '#ffffff',
+  'fc-white-2': '#dddddd',
+  'fc-gray-1': '#666666',
+  'fc-gray-2': '#888888',
+
+  //
+  'h-left': '#666666',
+  'h-background-color': '#f3f3f3',
+  'list-background-color': '#f3f3f3',
+};
+
+export default {
+  ...colors,
+};
