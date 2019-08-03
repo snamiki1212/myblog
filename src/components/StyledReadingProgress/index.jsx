f751bcf1ca0c0fddfe7250fe49b38cf3081b6825
snamiki1212 <snamiki1212@gmail.com>
2019-08-03 23:45:43 +0900

    Refactor to update css

--- src/components/StyledReadingProgress/index.jsx
+++ src/components/StyledReadingProgress/index.jsx
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
 import ReadingProgress from 'react-reading-progress';
-import './StyledReadingProgress.scss';
+import styled from 'styled-components';
 
 class StyledReadingProgress extends Component {
   render() {
     // MEMO: Target context class is 'target-el'
     return (
-      <div className="wrapper-reading-progress">
-        <ReadingProgress className="reading-progress" />
-      </div>
+      <Wrapper>
+        <RP />
+      </Wrapper>
     );
   }
 }
+
+const Wrapper = styled.div`
+  position: fixed;
+  z-index: 999;
+`;
+
+const RP = styled(ReadingProgress)`
+  position: relative !important; // MEMO: ライブラリ元に対して上書き
+  background: red !important; // TODO: なぜ動いているかわからない。。。red指定しているが、無効化されてグローバルに定義してるカラーが利用される。この１行を抜くとデフォルト値のカラーになってしまう。
+`;
 
 export default StyledReadingProgress;
