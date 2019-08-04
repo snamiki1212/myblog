0c7ed34e5e90f31c98034246c747c7d80b73633e
snamiki1212 <snamiki1212@gmail.com>
2019-08-04 15:05:28 +0900

    Refactor to remove css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import config from '../../../data/SiteConfig';
-import './Footer.scss';
 
 class Footer extends Component {
   render() {
@@ -8,26 +7,7 @@ class Footer extends Component {
     if (!copyright) {
       return null;
     }
-    return (
-      <footer className="footer">
-        <div className="notice-container">
-          <div className="copyright">
-            <h4>{copyright}</h4>
-          </div>
-
-          <div className="based-on">
-            <h4>
-              Based on
-              {' '}
-              <a href="https://github.com/Vagr9K/gatsby-material-starter">
-                Gatsby Material Starter
-              </a>
-              .
-            </h4>
-          </div>
-        </div>
-      </footer>
-    );
+    return <footer>{copyright}</footer>;
   }
 }
 
