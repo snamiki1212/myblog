5202ed432af58e957e4de0d4c3ec89135021dd75
snamiki1212 <snamiki1212@gmail.com>
2019-08-07 00:59:28 +0900

    Refactor logic

--- gatsbyjs/onCreateNode.ts
+++ gatsbyjs/onCreateNode.ts
@@ -3,6 +3,26 @@ import moment from 'moment';
 import siteConfig from '../data/SiteConfig';
 import _ from 'lodash';
 
+const generateSlug = ({node, parsedFilePath}): string => {
+  if (
+    Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+    Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
+  ) {
+    return `/${_.kebabCase(node.frontmatter.title)}`;
+  } else if (parsedFilePath.name !== 'index' && parsedFilePath.dir !== '') {
+    return `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
+  } else if (parsedFilePath.dir === '') {
+    return `/${parsedFilePath.name}/`;
+  } else if (
+    Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+    Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')
+  ) {
+    return `/${_.kebabCase(node.frontmatter.slug)}`;
+  } else {
+    return `/${parsedFilePath.dir}/`;
+  }
+};
+
 export const onCreateNode = (postNodes: any): any => ({
   node,
   actions,
@@ -11,40 +31,25 @@ export const onCreateNode = (postNodes: any): any => ({
   if (node.internal.type !== 'MarkdownRemark') return;
 
   const {createNodeField} = actions;
-  let slug;
+
   const fileNode = getNode(node.parent);
   const parsedFilePath = path.parse(fileNode.relativePath);
+  const slug = generateSlug({node, parsedFilePath});
 
   if (
     Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
-    Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
+    Object.prototype.hasOwnProperty.call(node.frontmatter, 'date')
   ) {
-    slug = `/${_.kebabCase(node.frontmatter.title)}`;
-  } else if (parsedFilePath.name !== 'index' && parsedFilePath.dir !== '') {
-    slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
-  } else if (parsedFilePath.dir === '') {
-    slug = `/${parsedFilePath.name}/`;
-  } else {
-    slug = `/${parsedFilePath.dir}/`;
-  }
-
-  if (Object.prototype.hasOwnProperty.call(node, 'frontmatter')) {
-    if (Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')) {
-      slug = `/${_.kebabCase(node.frontmatter.slug)}`;
+    const date = moment(node.frontmatter.date, siteConfig.dateFromFormat);
+    if (!date.isValid) {
+      console.warn(`WARNING: Invalid date.`, node.frontmatter); // eslint-disable-line
     }
 
-    if (Object.prototype.hasOwnProperty.call(node.frontmatter, 'date')) {
-      const date = moment(node.frontmatter.date, siteConfig.dateFromFormat);
-      if (!date.isValid) {
-        console.warn(`WARNING: Invalid date.`, node.frontmatter);
-      }
-
-      createNodeField({
-        node,
-        name: 'date',
-        value: date.toISOString(),
-      });
-    }
+    createNodeField({
+      node,
+      name: 'date',
+      value: date.toISOString(),
+    });
   }
 
   createNodeField({node, name: 'slug', value: slug});
