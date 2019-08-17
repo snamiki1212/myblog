24ff9f90c8bc13cac5cb0c1ba51048ad7c0a1bc9
snamiki1212 <snamiki1212@gmail.com>
2019-08-17 17:58:30 +0900

    Replace to material-ui

--- src/components/organisms/AboutCard.tsx
+++ src/components/organisms/AboutCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Card from 'react-md/lib/Cards/Card';
-import CardText from 'react-md/lib/Cards/CardText';
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
 import {MyLinks} from '../atoms/';
 import config from '../../../data/SiteConfig';
@@ -10,36 +12,61 @@ const myLinkedinUrl = config.userLinks.find(ele => ele.label === 'Linkedin')
   .url;
 
 export const AboutCard = (): JSX.Element => (
-  <Card className="md-grid md-cell--8">
-    <CardText>
+  <div style={{padding: '10px'}}>
+    <Paper>
       <h2>Nash プロフィール</h2>
       <Img src={config.userAvatar} alt={config.userName} />
-      <Text className="md-body-1">{config.userDescription}</Text>
-      <p>
-        日々の情報発信は Twitter を中心に行っています。 なにか連絡がある場合は
-        Twitter DM にして頂けると幸いです。
-        <br />
-        <a href={myTwitterUrl}>» Nash@Webエンジニア(@snamiki1212)</a>
-      </p>
+      <Text>{config.userDescription}</Text>
+      <Card>
+        <h2>活動</h2>
+        <p>
+          日々の情報発信は Twitter を中心に行っています。 なにか連絡がある場合は
+          Twitter DM にして頂けると幸いです。
+          <br />
+        </p>
+        <CardActions>
+          <Button>
+            <a href={myTwitterUrl}>» Nash@Webエンジニア(@snamiki1212)</a>
+          </Button>
+        </CardActions>
+      </Card>
 
-      <h2>経歴</h2>
-      <p>
-        詳細はLinkedInに記述していますので、こちらを参照願います。
-        <br />
-        <a href={myLinkedinUrl}>» LinkedIn(snamiki1212)</a>
-      </p>
-      <h2>その他</h2>
-      <Text className="md-body-2">
-        このブログのコンテンツは全てgithubで管理されています。
-        内容に問題や訂正すべき内容がある場合は
-        <a href={myTwitterUrl}>Twitter</a>
-        か直接
-        <a href={config.siteRepository}>このブログのリポジトリ</a>
-        にPRを出してもらえれば対応します。
-      </Text>
-    </CardText>
-    <MyLinks />
-  </Card>
+      <Card>
+        <h2>経歴</h2>
+        <p>
+          詳細はLinkedInに記述していますので、こちらを参照願います。
+          <br />
+        </p>
+        <CardActions>
+          <Button>
+            <a href={myLinkedinUrl}>» LinkedIn(snamiki1212)</a>
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card>
+        <h2>その他</h2>
+        <Text>
+          このブログのコンテンツは全てgithubで管理されています。
+          内容に問題や訂正すべき内容がある場合は
+          <a href={myTwitterUrl}>Twitter</a>
+          か直接
+          <a href={config.siteRepository}>ブログのリポジトリ</a>
+          にPRを出してもらえれば対応します。
+        </Text>
+        <CardActions>
+          <Button>
+            <a href={myTwitterUrl}>Twitter</a>
+          </Button>
+          <Button>
+            <a href={config.siteRepository}>ブログのリポジトリ</a>
+          </Button>
+        </CardActions>
+      </Card>
+
+      <MyLinks />
+    </Paper>
+  </div>
 );
 
 const Img = styled.img`
