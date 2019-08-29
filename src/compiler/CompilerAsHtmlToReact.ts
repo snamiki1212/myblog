f0d7a7f12a91a3d18b2a9cf348bdd27f6d9eec25
snamiki1212 <snamiki1212@gmail.com>
2019-08-29 20:07:11 +0900

    Update comment

--- src/compiler/CompilerAsHtmlToReact.ts
+++ src/compiler/CompilerAsHtmlToReact.ts
@@ -129,7 +129,16 @@ const Li = styled.li`
   padding: 10px
 `;
 
-// Compier: html >> react
+/**
+ * Markdown のコンテンツのパーサーの結果をReactComponentに変換
+ *
+ * - RemarkのParser：Markdown >> html
+ * - このCompier: html >> react
+ *
+ * ## このCompilerの導入モチベーション
+ * - CSS: コンポーネント志向でCSSを書きたいので、このCompilerを導入。このCompilerがないと、scss とかを import しないとならなさそう。
+ * - AMP: img → am-img に変換する予定だった。ただ、導入コストが高い＋GatsbyJSの公式でAMPを一級市民になりそうな流れがありそうなので、一旦放置。
+ */
 export const renderReact = new RehypeReact({
   createElement: React.createElement,
   components: {
