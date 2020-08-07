f107bde46569683d0f156873cf823ab5120a1893
snamiki1212 <snamiki1212@gmail.com>
2020-08-08 00:37:15 +0900

    Refactor components

--- src/components/atoms/Markdown.tsx
+++ src/components/atoms/Markdown.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import renderReact from '../../compiler/CompilerAsHtmlToReact';
 
 type Props = {htmlAst: any};
 
 export const Markdown: React.FC<Props> = ({htmlAst}) => {
-  return (
-    <div
-      style={{
-        wordWrap: 'break-word',
-        width: '100%',
-      }}
-    >
-      {renderReact(htmlAst)}
-    </div>
-  );
+  return <Container>{renderReact(htmlAst)}</Container>;
 };
+
+const Container = styled.div`
+  word-wrap: break-word;
+  width: 100%;
+`;
