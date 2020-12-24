a6aca23a262eff7cd82d490ea5ef250c2f0e1324
snamiki1212 <snamiki1212@gmail.com>
2020-12-23 16:23:55 -0800

    Update test

--- src/lib/validator.test.ts
+++ src/lib/validator.test.ts
@@ -1,6 +1,37 @@
-// import {isInternalPageLink, isAffiLink} from './validator';
+import {isInternalPageLink, isAffiLink} from './validator';
 
-const sum = (a, b) => a + b;
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+const HTTPS_URL = 'https://google.com';
+const HTTP_URL = 'http://wwwa.cao.go.jp/notice/20191101notice.html';
+
+describe('func:isInternalPageLink', () => {
+  test('yes', () => {
+    const _true = isInternalPageLink('./test-page');
+    expect(_true).toBeTruthy();
+  });
+
+  test('no', () => {
+    const _false1 = isInternalPageLink(HTTPS_URL);
+    const _false2 = isInternalPageLink(HTTP_URL);
+    const _false3 = isInternalPageLink('test.jpg'); // media
+
+    expect(_false1).toBeFalsy();
+    expect(_false2).toBeFalsy();
+    expect(_false3).toBeFalsy();
+  });
+});
+
+// 一部のアフィリンクのURLスタートが "//" スタートのがある。
+describe('func:isAffiLink', () => {
+  test('yes', () => {
+    const affiLink = '//ad.jp.ap.valuecommerce.com/servlet/gifbanner';
+    expect(isAffiLink(affiLink)).toBeTruthy();
+  });
+
+  test('no', () => {
+    const _false1 = isAffiLink(HTTPS_URL);
+    const _false2 = isAffiLink(HTTP_URL);
+
+    expect(_false1).toBeFalsy();
+    expect(_false2).toBeFalsy();
+  });
 });
