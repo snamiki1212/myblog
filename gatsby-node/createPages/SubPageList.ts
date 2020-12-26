0180d0af4b4194692fbb6c0e7dc05e628a0e4e8b
snamiki1212 <snamiki1212@gmail.com>
2020-12-25 16:07:52 -0800

    Refacotr createPage logic

---
+++ gatsby-node/createPages/SubPageList.ts
@@ -0,0 +1,117 @@
+import kebabCase from 'lodash.kebabcase';
+import {TagPage, CategoryPage, POSTS_PER_PAGE} from './constants';
+import {calcPaginationNum} from './utils';
+
+import {SubPageProps, CategoryPageContext, TagPageContext} from '../types';
+
+type CreatePagesArgs = {
+  createPage: any;
+  subPageContext: any;
+};
+
+export class SubPageList {
+  public type: 'category' | 'tag';
+  public subPageList: SubPageProps[] = [];
+
+  constructor(type: 'category' | 'tag') {
+    this.type = type;
+  }
+
+  private generatePath = (name: string): string => {
+    switch (this.type) {
+      case 'category':
+        return `/categories/${kebabCase(name)}`;
+      case 'tag':
+        return `/tags/${kebabCase(name)}`;
+      default:
+        throw new Error('unexpected type error');
+    }
+  };
+
+  public createPages = (params: CreatePagesArgs) => {
+    switch (this.type) {
+      case 'category': {
+        return this.createCategoryPages(params);
+      }
+      case 'tag': {
+        return this.createTagPages(params);
+      }
+    }
+  };
+
+  private createTagPages = ({createPage, subPageContext}: CreatePagesArgs) => {
+    this.subPageList.forEach((subPage) => {
+      const lastCategoryPage = calcPaginationNum(subPage.count);
+      Array.from({length: lastCategoryPage}).forEach((_x, i) => {
+        // Paginationごとにループ
+        const tagPageContext: TagPageContext = {
+          // Pagination Context
+          limit: POSTS_PER_PAGE,
+          skip: i * POSTS_PER_PAGE,
+          lastPage: lastCategoryPage,
+          currentPage: i + 1,
+          // SubPageContext
+          ...subPageContext,
+          //
+          tag: subPage.name,
+        };
+
+        createPage({
+          path: `${subPage.path}/${i === 0 ? '' : i + 1}`,
+          component: TagPage,
+          context: tagPageContext,
+        });
+      });
+    });
+  };
+
+  private createCategoryPages = ({
+    createPage,
+    subPageContext,
+  }: CreatePagesArgs) => {
+    this.subPageList.forEach((subPage) => {
+      const lastCategoryPage = calcPaginationNum(subPage.count);
+
+      Array.from({length: lastCategoryPage}).forEach((_x, idx) => {
+        const context: CategoryPageContext = {
+          // Pagination
+          limit: POSTS_PER_PAGE,
+          skip: idx * POSTS_PER_PAGE,
+          lastPage: lastCategoryPage,
+          currentPage: idx + 1,
+          // SubPageContext
+          ...subPageContext,
+          //
+          category: subPage.name,
+        };
+
+        createPage({
+          path: `${subPage.path}/${idx === 0 ? '' : idx + 1}`,
+          component: CategoryPage,
+          context: context,
+        });
+      });
+    });
+  };
+
+  public incrementOrAddPage = (name: string): void => {
+    if (!name) return;
+    const path = this.generatePath(name);
+    const maybeIndex = this.subPageList.findIndex(
+      (subPage) => subPage.name === name
+    );
+    const exist = maybeIndex !== -1;
+
+    if (exist) {
+      // INCREMENT: 既存の項目
+      this.subPageList[maybeIndex] = {
+        ...this.subPageList[maybeIndex],
+        count: ++this.subPageList[maybeIndex].count,
+        path,
+      };
+    } else {
+      // ADD: 新しく項目を追加
+      this.subPageList = [...this.subPageList, {name, count: 1, path}];
+    }
+  };
+}
---
+++ gatsby-node/createPages/index.ts
@@ -0,0 +1,165 @@
+import {SubPageContext, IndexPageContext, PostPageContext} from '../types';
+import {fetchRandoms, calcPaginationNum} from './utils';
+import {SubPageList} from './SubPageList';
+import {
+  POSTS_PER_PAGE,
+  POSTS_AS_SUGGESTION,
+  PostPage,
+  IndexPage,
+} from './constants';
+
+const allMarkdownRemarkGraphQL = `
+{
+  allMarkdownRemark {
+    edges {
+      node {
+        id
+        frontmatter {
+          tags
+          category
+        }
+        fields {
+          _slug
+        }
+      }
+    }
+  }
+}
+`;
+
+type AllMarkdownRemarkResult = {
+  allMarkdownRemark: {
+    edges: {
+      node: {
+        id: string;
+        frontmatter: {
+          id: string;
+          tags: string[];
+          category: string;
+        };
+        fields: {
+          _slug: string; // TODO: 消して、他のslug を使いたい
+        };
+      };
+    }[];
+  };
+};
+
+const createPostPages = ({allPosts, createPage, subPageContext}) => {
+  allPosts.forEach((edge) => {
+    const category = edge.node.frontmatter.category;
+
+    const allSuggestions = allPosts.filter(
+      (post) =>
+        post.node.frontmatter.category === category &&
+        post.node.id !== edge.node.id
+    );
+
+    const selectedSuggestions: typeof allPosts = fetchRandoms(
+      allSuggestions,
+      POSTS_AS_SUGGESTION
+    );
+
+    const suggestionNodeIDs = selectedSuggestions.map(
+      (suggestion) => suggestion.node.id
+    );
+
+    const postPageCtx: PostPageContext = {
+      // SubPageContext
+      ...subPageContext,
+      // etc
+      slug: edge.node.fields._slug,
+      suggestionNodeIDs,
+    };
+
+    createPage({
+      path: edge.node.fields._slug,
+      component: PostPage,
+      context: postPageCtx,
+    });
+  });
+};
+
+const createIndexPages = ({createPage, subPageContext, length}) => {
+  Array.from({length}).forEach((_, idx): void => {
+    const indexPageContext: IndexPageContext = {
+      // Pagination Context
+      limit: POSTS_PER_PAGE,
+      skip: idx * POSTS_PER_PAGE,
+      lastPage: length,
+      currentPage: idx + 1,
+      // SubPage Context
+      ...subPageContext,
+      // etc
+    };
+
+    createPage({
+      path: idx === 0 ? '/' : `/${idx + 1}`,
+      component: IndexPage,
+      context: indexPageContext,
+    });
+  });
+};
+
+const makeSubPageList = ({allPosts}: {allPosts: any[]}) => {
+  const {categories, tags} = allPosts.reduce<{
+    categories: SubPageList;
+    tags: SubPageList;
+  }>(
+    ({categories, tags}, edge) => {
+      const {tags: edgeTags, category: edgeCategory} = edge.node.frontmatter;
+
+      // tags
+      edgeTags.forEach((tag: string): void => {
+        tags.incrementOrAddPage(tag);
+      });
+
+      // category
+      categories.incrementOrAddPage(edgeCategory);
+
+      return {categories, tags};
+    },
+    {
+      categories: new SubPageList('category'),
+      tags: new SubPageList('tag'),
+    }
+  );
+
+  return {categories, tags};
+};
+
+export const createPages = async ({graphql, actions}) => {
+  const {createPage} = actions;
+
+  return (async () => {
+    try {
+      const result = await graphql(allMarkdownRemarkGraphQL);
+
+      // GraphQL data
+      const data = result.data as AllMarkdownRemarkResult;
+      const {allMarkdownRemark} = data;
+      const {edges: allPosts} = allMarkdownRemark;
+
+      // build
+      const lastPostPage = calcPaginationNum(allPosts.length);
+      const {categories, tags} = makeSubPageList({allPosts});
+      const subPageContext: SubPageContext = {
+        categories: categories.subPageList,
+        tags: tags.subPageList,
+      };
+
+      // create Pages
+      categories.createPages({createPage, subPageContext});
+      tags.createPages({createPage, subPageContext});
+      createIndexPages({createPage, subPageContext, length: lastPostPage});
+      createPostPages({createPage, subPageContext, allPosts});
+
+      return;
+    } catch (errors) {
+      /* eslint no-console: "off" */
+      console.log('-----------------------');
+      console.error(errors);
+      throw errors;
+    }
+  })();
+};
