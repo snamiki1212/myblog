f123161271728e8480f88a61985ebdd2c0180bab
snamiki1212 <snamiki1212@gmail.com>
2020-12-27 23:14:56 -0800

    Update header link

--- src/components/molecules/HeadNav.tsx
+++ src/components/molecules/HeadNav.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import {colors} from '../../../data/color';
-import {siteTitle} from '../../../data/SiteConfig'
+import {siteTitle} from '../../../data/SiteConfig';
+import {Link} from 'gatsby';
 
 export const HeadNav: React.FC = () => {
   return (
     <Wrapper>
-      <span>
-        <Image src="/blogcard.png" />
-      </span>
-      <BlogTitle>{siteTitle}</BlogTitle>
+      <Link to="/">
+        <Flex>
+          <span>
+            <Image src="/blogcard.png" />
+          </span>
+          <BlogTitle>{siteTitle}</BlogTitle>
+        </Flex>
+      </Link>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   background: ${colors.baseDark};
+`;
+
+const Flex = styled.div`
   display: flex;
   align-items: center;
 `;
@@ -23,9 +31,9 @@ const Wrapper = styled.div`
 const Image = styled.img`
   width: 100px;
   height: 100px;
-`
+`;
 
 const BlogTitle = styled.span`
   color: ${colors.primaryVivid};
   font-size: 54px;
-`
+`;
