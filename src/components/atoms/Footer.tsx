97d3b9432f71c77f2bcc5117d9bd724785e2011f
snamiki1212 <snamiki1212@gmail.com>
2020-12-27 23:59:37 -0800

    Update footer

--- src/components/atoms/Footer.tsx
+++ src/components/atoms/Footer.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import config from '../../../data/SiteConfig';
 import {colors} from '../../../data/color';
+import {Logo} from './Logo';
+import {Link} from 'gatsby';
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,26 +12,86 @@ const Wrapper = styled.div`
   flex-direction: column;
 
   height: 500px;
+  padding: 0 20px;
 
   color: ${colors.DEPRECATED_grayLight};
-  background: ${colors.DEPRECATED_black1};
+  background: ${colors.baseDark};
 `;
 
-const Navigations = () => {
-  return <div>
+const getURL = (snsName: 'github' | 'twitter' | 'linkedin'): string =>
+  config.mySocials?.find((_) => _.icon === snsName)?.url ?? '';
 
-  </div>
-}
+const Navigations = () => {
+  const githubURL = getURL('github');
+  const twitterURL = getURL('twitter');
+  const linkedinURL = getURL('linkedin');
+
+  return (
+    <NavWrapper>
+      <Box>
+        <Link to="/">
+          <Logo size={146} />
+        </Link>
+      </Box>
+      <Box>
+        <PrimaryText>Lunash</PrimaryText>
+        <LinkText to="/about">経歴・仕事依頼</LinkText>
+      </Box>
+      <Box>
+        <PrimaryText>Blog</PrimaryText>
+        <LinkText to="categories">カテゴリ 一覧</LinkText>
+        <LinkText to="tags">タグ 一覧</LinkText>
+      </Box>
+      <Box>
+        <PrimaryText>SNS</PrimaryText>
+        <LinkText to={twitterURL}>Twitter</LinkText>
+        <LinkText to={githubURL}>Github</LinkText>
+        <LinkText to={linkedinURL}>Linkedin</LinkText>
+      </Box>
+    </NavWrapper>
+  );
+};
+
+const NavWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 50px 0;
+  width: 100%;
+`;
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const PrimaryText = styled.div`
+  color: ${colors.primaryVivid};
+`;
+const LinkText = styled(Link)`
+  color: ${colors.baseLight};
+`;
 
 export const Footer: React.FC = () => {
   return (
     <Wrapper>
       <Navigations />
-      <Copyright />
+      <Bottom>
+        <HR />
+        <Copyright>{config.copyright}</Copyright>
+      </Bottom>
     </Wrapper>
   );
 };
 
-const Copyright: React.FC = () => {
-  return <footer>{config.copyright}</footer>;
-};
+const Bottom = styled.div`
+  width: 80%;
+`
+
+const HR = styled.hr`
+  border: 1px solid ${colors.primaryVivid};
+`;
+
+const Copyright = styled.footer`
+  color: ${colors.baseLight};
+  padding: 10px 0;
+`;
