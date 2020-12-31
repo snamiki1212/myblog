9c3d36026ee9a1bcd443e718bf4a5eeb8125548e
snamiki1212 <snamiki1212@gmail.com>
2020-12-30 23:23:24 -0800

    Update design

--- src/components/molecules/HeadNav.tsx
+++ src/components/molecules/HeadNav.tsx
@@ -1,33 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import {siteTitle} from '../../../data/SiteConfig';
+import {mixinSpinAnimate} from '../atoms/SpinAnimation';
 import {Link} from 'gatsby';
 
 export const HeadNav: React.FC = () => {
   return (
     <Wrapper>
-      
-        <Flex>
-          
-          <span>
-            <Link to="/">
-              <Image src="/blogcard.png" />
-            </Link>
-          </span>
+      <Flex>
+        <Link to="/">
+          <HoverForSpinArea>
+            <Image src="/blogcard.png" />
+          </HoverForSpinArea>
+        </Link>
 
-          <Link to="/">
-            <BlogTitle>{siteTitle}</BlogTitle>
-          </Link>
-          
-          
-        </Flex>
-      
+        <Link to="/">
+          <BlogTitle>{siteTitle}</BlogTitle>
+        </Link>
+      </Flex>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  background: ${props => props.theme.color.baseDark};
+  background: ${(props) => props.theme.color.baseDark};
 `;
 
 const Flex = styled.div`
@@ -40,8 +36,16 @@ const Image = styled.img`
   height: 100px;
 `;
 
+const HoverForSpinArea = styled.div`
+  & :hover {
+    ${Image} {
+      ${mixinSpinAnimate}
+    }
+  }
+`;
+
 const BlogTitle = styled.span`
-  color: ${props => props.theme.color.primaryVivid};
-  font-family: ${props => props.theme.fontFamily.primary}px;
-  font-size: ${props => props.theme.fontSize.tmp_3}px;
+  color: ${(props) => props.theme.color.primaryVivid};
+  font-family: ${(props) => props.theme.fontFamily.primary}px;
+  font-size: ${(props) => props.theme.fontSize.tmp_3}px;
 `;
