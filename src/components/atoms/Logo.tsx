be4546ba96af169d7188a135a33d0f3185efa244
snamiki1212 <snamiki1212@gmail.com>
2021-01-02 23:23:51 -0800

    Fix merge process

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export const Logo: React.FC<{size: number}> = ({size}) => {
-<<<<<<< HEAD
-  return <img src="/blogcard.png" style={{width: size, height: size}} />;
-=======
-  return <img src="/blogcard.png" style={{width: size, height: size}} alt="logo-img" />;
->>>>>>> starter/master
+  return (
+    <img
+      src="/blogcard.png"
+      style={{width: size, height: size}}
+      alt="logo-img"
+    />
+  );
 };
