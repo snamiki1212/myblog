e235cb29b3ef2b80d9f110985bb7b9ba24d32fe0
snamiki1212 <snamiki1212@gmail.com>
2021-01-10 15:37:29 -0800

    Update social lilnks design

--- src/components/atoms/MyLinks.tsx
+++ src/components/atoms/MyLinks.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Icon} from './';
-import { Link } from 'gatsby'
+import {Link} from 'gatsby';
 
 import styled from 'styled-components';
 import config from '../../../data/SiteConfig';
@@ -9,16 +9,18 @@ export const MyLinks: React.FC = () => {
   const {mySocials} = config;
 
   if (!Array.isArray(mySocials)) {
-    console.warn("My social data in config must be array.");
+    console.warn('My social data in config must be array.');
     return <></>;
-  };
+  }
 
   return (
     <Wrapper>
-      {mySocials.map(social => (
-        <_Link key={social.icon} to={social.url} aria-label={social.icon}>
-          <Icon icon={social.icon} />
-        </_Link>
+      {mySocials.map((social) => (
+        <InnerWrapper key={social.icon}>
+          <Link key={social.icon} to={social.url} aria-label={social.icon}>
+            <Icon icon={social.icon} />
+          </Link>
+        </InnerWrapper>
       ))}
     </Wrapper>
   );
@@ -26,6 +28,13 @@ export const MyLinks: React.FC = () => {
 
 const Wrapper = styled.div``;
 
-const _Link = styled(Link)`
-  margin-right: 7px;
+const InnerWrapper = styled.span`
+  transition: 0.5s;
+  padding: 5px;
+  border-radius: 50%;
+  display: inline-block;
+  & :hover {
+    transition: 0.5s;
+    background-color: ${(props) => props.theme.color.primaryVivid};
+  }
 `;
