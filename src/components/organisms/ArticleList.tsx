6ee4ebdf6daa77d1a6cac7ad2e8c9f8797b8ad6c
snamiki1212 <snamiki1212@gmail.com>
2021-01-25 21:08:25 -0800

    Update article-list

--- src/components/organisms/ArticleList.tsx
+++ src/components/organisms/ArticleList.tsx
@@ -27,8 +27,7 @@ export const Wrapper = styled.div`
 `;
 
 export const Child = styled.div`
-  min-width: 350px;
-  width: 46%;
+  flex: 1 0 400px;
   margin: 2%;
   border-radius: 10px;
   box-shadow: 2px 2px 10px lightgrey;
