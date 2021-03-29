7a5dd2096080808856f7eedd410737530449d510
snamiki1212 <snamiki1212@gmail.com>
2021-03-28 23:07:57 -0700

    Update dummy image handler

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Img from 'gatsby-image';
+import {DummyImage} from './DummyImage';
 
 type Props = {
   imgInfo: {
@@ -11,15 +12,21 @@ type Props = {
 
 const Image: React.FC<Props> = ({imgInfo, ...rest}) => {
   try {
+    if (!imgInfo) {
+      return <DummyImage {...rest} />;
+    }
+
     const {publicURL, childImageSharp} = imgInfo;
 
     if (!childImageSharp) {
-      console.warn("cannot find childImageSharp");
+      console.warn('Cannot find childImageSharp on Image Component');
       return <img src={publicURL} alt="this-is-image" {...rest} />;
     }
+
     return <Img fluid={childImageSharp.fluid} {...rest} />;
-  } catch (e) {
-    return <img src={'/eyecatch.webp'} alt="eyecatch" style={{width: '100%'}} />;
+  } catch (err) {
+    console.error('Something error happen on Image Component');
+    return <span {...rest}>img</span>;
   }
 };
 
