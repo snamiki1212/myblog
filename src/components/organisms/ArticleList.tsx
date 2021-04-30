78c2f467842adc022fa60c62039833f7d20b099f
snamiki1212 <snamiki1212@gmail.com>
2021-04-29 18:17:32 -0700

    Update article-list UI

--- src/components/organisms/ArticleList.tsx
+++ src/components/organisms/ArticleList.tsx
@@ -22,7 +22,6 @@ export const ArticleList: React.FC<Props> = ({postEdges}) => {
 const Container = styled.div`
   display: grid;
   gap: 1rem;
-  margin: 1rem 0;
 `;
 
 const InnerContainer = styled.div`
--- src/components/organisms/ArticleListLayout.tsx
+++ src/components/organisms/ArticleListLayout.tsx
@@ -8,39 +8,50 @@ type Props = {
   pagination: React.ReactNode;
 };
 
-export const ArticleListLayout: React.FC<Props> = ({articleListHeader, articles, profile, pagination}) => {
+export const ArticleListLayout: React.FC<Props> = ({
+  articleListHeader,
+  articles,
+  profile,
+  pagination,
+}) => {
   return (
     <Wrapper>
-      {articleListHeader && <ArticleListHeader>{articleListHeader}</ArticleListHeader>}
-      <ArticlesArea>{articles}</ArticlesArea>
-      <PaginationArea>{pagination}</PaginationArea>
-      <ProfileArea>{profile}</ProfileArea>
+      {articleListHeader && {articleListHeader}}
+      <ArticleAndSidebar>
+        <ArticleArea>
+          {articles}
+          {pagination}
+        </ArticleArea>
+        <ProfileSection>{profile}</ProfileSection>
+      </ArticleAndSidebar>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  align-content: space-between;
-  flex-wrap: wrap;
-  margin: 0 ${(props) => props.theme.layout.marginVertical}px;
+  display: grid;
+  gap: 1rem;
+  padding: ${(props) => props.theme.layout.marginVertical}px;
 `;
 
-const ArticleListHeader = styled.div`
-  width: 100%;
+const ArticleAndSidebar = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 300px;
+  gap: 2rem;
 `;
 
-const ArticlesArea = styled.div`
-  width: 100%;
+const ArticleArea = styled.div`
+  grid-column: 1 / 2;
+  @media (max-width: 700px) {
+    /* // TODO: dont use magic num */
+    grid-column: 1 / 3;
+  }
 `;
 
-const PaginationArea = styled.div`
-  width: 100%;
-`;
-
-const ProfileArea = styled.div`
-  width: 100%;
+const ProfileSection = styled.div`
+  grid-column: span 1;
+  @media (max-width: 700px) {
+    /* // TODO: dont use magic num */
+    grid-column: span 2;
+  }
 `;
