252c5bd8f21e26d4c20ffc8b68f06298b6b772b0
snamiki1212 <snamiki1212@gmail.com>
2021-07-31 22:47:56 -0700

    Update markdown logic

--- src/compiler/CompilerAsHtmlToReact.tsx
+++ src/compiler/CompilerAsHtmlToReact.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import RehypeReact from 'rehype-react';
+import {unified} from 'unified';
+import Rehype2react from 'rehype-react';
+import HtmlParser from 'rehype-parse';
 import {components} from './components';
 
-/**
- * Markdown のコンテンツのパーサーの結果をReactComponentに変換
- *
- * - RemarkのParser：Markdown >> html
- * - このCompier: html >> react
- *
- * ## このCompilerの導入モチベーション
- * - CSS: コンポーネント志向でCSSを書きたいので、このCompilerを導入。このCompilerがないと、scss とかを import しないとならなさそう。
- * - AMP: img → am-img に変換する予定だった。ただ、導入コストが高い＋GatsbyJSの公式でAMPを一級市民になりそうな流れがありそうなので、一旦放置。
- */
-export const renderReact = new RehypeReact({
+const rehype2reactOption = {
   createElement: React.createElement,
   components,
-}).Compiler;
+};
 
-export default renderReact;
+const processor = unified()
+  // parser
+  .use(HtmlParser)
+  // transformer
+  .use(Rehype2react, rehype2reactOption);
+
+export const renderReactFromHtml = ({html}) =>
+  processor.processSync(html).result;
