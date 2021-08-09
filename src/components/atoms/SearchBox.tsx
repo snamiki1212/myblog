dcb66409a84e4fca71509db4ea7bad3e7b849f98
snamiki1212 <snamiki1212@gmail.com>
2021-08-09 12:46:08 -0700

    Update search box UI

--- src/components/atoms/SearchBox.tsx
+++ src/components/atoms/SearchBox.tsx
@@ -6,9 +6,10 @@ type Props = {searchEngineId: string};
 export const SearchBox = ({searchEngineId}: Props) => {
   return (
     <form id="cse-search-box" action="https://google.com/cse" target="_blank">
+      <span className="magnify-icon">🔍</span>
       <input type="hidden" name="cx" value={searchEngineId} />
       <input type="hidden" name="ie" value="UTF-8" />
-      <input type="text" name="q" placeholder="🔍" />
+      <input type="text" name="q" placeholder="Search Text" />
       {/* <input type="submit" name="sa" value="🔍" /> */}
     </form>
   );
