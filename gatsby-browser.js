a11b67e896e5bf3d628f36485b8a05c68e2ee20f
snamiki1212 <snamiki1212@gmail.com>
2021-09-02 03:02:23 -0700

    Update markdown UI

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 // for prismjs. readme: https://github.com/PrismJS/prism/tree/master/themes
 require('prismjs/themes/prism-okaidia.css');
+
 import './src/global.scss';
 import './src/affi.scss';
+import './src/markdown.scss';
