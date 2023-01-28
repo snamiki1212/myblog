652a5385a23914c8b32a34df82eef6a53e363f4d
snamiki1212 <snamiki1212@gmail.com>
2023-01-29 00:51:45 +0900

    fix: update theme color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: ["cupcake"],
+  },
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
