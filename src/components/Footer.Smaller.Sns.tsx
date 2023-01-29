f5d888965635bf159a47ad03718ec86117886aea
snamiki1212 <snamiki1212@gmail.com>
2023-01-29 11:07:31 +0900

    refactor: replace react with astro

--- src/components/Footer.Smaller.Sns.tsx
+++ src/components/Footer.Smaller.Sns.tsx
@@ -1,5 +1,8 @@
 import { SNS_LIST } from "../constants/sns";
 
+/**
+ * Use React component instead of Astro because `sns.renderSvg()` does not work when using .astro file.
+ */
 export const FooterSmallerSns = () => {
   return SNS_LIST.map((sns) => (
     <a href={sns.url} target="_blank">
