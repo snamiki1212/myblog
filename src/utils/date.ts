6f2fdb6b86156b987c842e8db946ebe3cf83096a
snamiki1212 <snamiki1212@gmail.com>
2023-01-29 13:51:47 +0900

    fix: update post-card style

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,17 @@
+import type { CollectionEntry } from "astro:content";
+type BlogEntry = CollectionEntry<"blog">;
+
 export const renderDate = (dateStr: string): string => {
   const d = new Date(dateStr);
   return d.toISOString().split('T')[0];
+}
+
+export const renderOnlyOneDate = (entry: BlogEntry): { type: "createdAt" | "updatedAt", title: string } => {
+  const createdAt = entry.data.createdAt
+  const updatedAt = entry.data.updatedAt
+  if (createdAt === updatedAt) {
+    return { type: "createdAt", title: renderDate(createdAt) }
+  } else {
+    return { type: "updatedAt", title: renderDate(updatedAt) }
+  }
 }
