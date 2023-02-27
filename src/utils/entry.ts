cb0def041728982c189fe5051d0315d8c8afe6e2
snamiki1212 <snamiki1212@gmail.com>
2023-02-27 16:53:45 +0900

    refactor: update entry list logic

--- src/utils/entry.ts
+++ src/utils/entry.ts
@@ -1,7 +1,17 @@
-import type { CollectionEntry } from "astro:content";
-import { sortByCreatedAt } from "./meta";
 import * as R from "remeda";
+import type { CollectionEntry } from "astro:content";
+import { toPostListUrl, toCategoryIndexUrl } from "../utils/url";
+import { sortByCreatedAt } from "./meta";
+import { LATEST_PAGE_SIZE } from "../constants/site";
+
 type BlogEntry = CollectionEntry<"blog">;
+
+type PickedEntryInfo = {
+  title: string;
+  total: number;
+  entires: BlogEntry[];
+  nextUrl: string;
+};
 
 export const selectRelatedEntryList = (entry: BlogEntry, candidateEntryList: BlogEntry[]): BlogEntry[] => {
   return R.pipe(
@@ -12,3 +22,37 @@ export const selectRelatedEntryList = (entry: BlogEntry, candidateEntryList: Blo
     R.take(4),
   )
 }
+
+export const toPickedEntryInfoList = (entires: BlogEntry[], opt: { pageSize: number }): PickedEntryInfo[] => {
+  const [latestEntries, remainingEntires] = R.splitAt(
+    entires,
+    opt.pageSize,
+  );
+  const obj = remainingEntires.reduce((prev, current) => {
+    const category = current.data.category;
+    if (prev[category]) {
+      prev[category].push(current);
+    } else {
+      prev[category] = [current];
+    }
+    return prev;
+  }, {} as Record<string, BlogEntry[]>);
+  const list = Object.entries(obj)
+    .map(([category, entries]) => ({
+      title: category,
+      entries,
+      total: entries.length,
+      nextUrl: toCategoryIndexUrl(category),
+    }))
+    .map((item) => ({
+      ...item,
+      entires: R.take(item.entries, LATEST_PAGE_SIZE),
+    }));
+  const latest: PickedEntryInfo = {
+    title: "新着",
+    total: entires.length,
+    nextUrl: toPostListUrl(1),
+    entires: latestEntries,
+  };
+  return [latest, ...list];
+};
