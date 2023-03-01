b7631cebfe7c3ef71040221a96054aa71da19532
snamiki1212 <snamiki1212@gmail.com>
2023-03-01 12:50:47 +0900

    fix: update logo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   daisyui: {
-    themes: ["cupcake"],
+    themes: ["pastel"],
   },
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
