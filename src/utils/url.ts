d83c42f01debfcf2747313e242ae124d8baf6910
snamiki1212 <snamiki1212@gmail.com>
2023-08-12 07:02:56 +0900

    fix: update design

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,9 +1,23 @@
-export const toCategoryIndexUrl = (name: string): string => `/categories/${name}`;
-export const toCategoryListUrl = (): string => `/categories`;
+/**
+ * Category
+ */
+export const toCategoryIndexUrl = (name: string) => `/categories/${name}` as const;
+export const toCategoryListUrl = () => `/categories` as const;
 
-export const toTagIndexUrl = (name: string): string => `/tags/${name}`;
-export const toTagListUrl = (): string => `/tags`;
+/**
+ * Tag
+ */
+export const toTagIndexUrl = (name: string) => `/tags/${name}` as const;
+export const toTagListUrl = () => `/tags` as const;
 
-export const toPostListUrl = (page: number): string => `/${page}`;
+/**
+ * Post
+ */
+export const toPostListUrl = (page: number) => `/${page}` as const;
 
-export const toHomeUrl = (): string => "/";
+export const toPostUrl = (slug: string) => `/${slug}` as const;
+
+/**
+ * Home
+ */
+export const toHomeUrl = () => "/" as const;
