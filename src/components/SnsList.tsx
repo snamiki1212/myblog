5195f117fbaf11c5fe478fc2af8f92b83ead034a
snamiki1212 <snamiki1212@gmail.com>
2023-08-11 22:55:43 +0900

    refactor: update sns component

--- src/components/SnsList.tsx
+++ src/components/SnsList.tsx
@@ -3,7 +3,7 @@ import { SNS_LIST } from "../constants/sns";
 /**
  * Use React component instead of Astro because `sns.renderSvg()` does not work when using .astro file.
  */
-export const FooterSmallerSns = () => {
+export const SnsList = () => {
   return SNS_LIST.map((sns) => (
     <a href={sns.url} target="_blank" className="btn glass">
       <div>{sns.renderSvg()}</div>
