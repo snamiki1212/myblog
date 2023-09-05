7955860a0add2202c40eb6cd065be7aa3de3ef5d
snamiki1212 <snamiki1212@gmail.com>
2023-09-05 10:21:12 +0900

    tmp: add transition config but not working

---
+++ src/utils/viewtransition.ts
@@ -0,0 +1,7 @@
+/**
+ * @link https://developer.chrome.com/blog/astro-view-transitions/
+ * @link https://docs.astro.build/en/guides/view-transitions/
+ */
+export const TRANSITION_NAME = {
+  ARTICLE: "article",
+}
