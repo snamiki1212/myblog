aa72b554b97ce3784093bf68372c9211b790a643
snamiki1212 <snamiki1212@gmail.com>
2023-09-06 01:10:01 +0900

    fix: workaround view transition

---
+++ src/components/Container.WorkaroundViewTransition.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * WORKAROUND
+ *
+ * I want to use Container.astro but view transition does not work because unknown behavior,
+ * so useing this wrapped component by React.
+ */
+export const Container = ({ children }) => {
+  return <div class="max-w-screen-lg m-auto md:px-8">{children}</div>;
+};
