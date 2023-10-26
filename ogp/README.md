d274fecb31335ad399381b8ff5cadcc14f96e3f2
snamiki1212 <snamiki1212@gmail.com>
2023-10-26 09:39:46 +0900

    fix: add ogp

---
+++ ogp/README.md
@@ -0,0 +1,6 @@
+Create ogp manually.
+
+1. Open [Vercel OG Image Playground](https://og-playground.vercel.app/)
+2. Copy `<div>~</div>` at `./ogp.tsx`` and paste to playground
+3. Edit and save to `/images/ogp.png`
+4. Use ogp at `src/layouts/BaseLayout.astro`
---
+++ ogp/ogp.tsx
@@ -0,0 +1,23 @@
+const fn = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#fff",
+        fontSize: 32,
+        fontWeight: 600,
+        backgroundColor: "hsla(240, 100%, 80%, 1)",
+        background: "linear-gradient(150deg, #e5e5ff, #9999ff)",
+        color: "#303030",
+      }}
+    >
+      <div style={{ fontSize: 64 }}>Lunash</div>
+      <div style={{ marginTop: 40, color: "#696969" }}>snamiki1212.com</div>
+    </div>
+  );
+};
