391084ae2ea95c894f95c906faf34c48a2555751
snamiki1212 <snamiki1212@gmail.com>
2024-01-10 09:52:36 +0900

    refactor: update getter

--- src/utils/astro.ts
+++ src/utils/astro.ts
@@ -1,5 +1,14 @@
-import { type CollectionEntry } from "astro:content";
+import { type CollectionEntry, getEntryBySlug } from "astro:content";
 
+/****************************
+ * Types
+ ****************************/
 export type BlogEntry = CollectionEntry<"blog">;
 
 export type AboutEntry = CollectionEntry<"about">;
+
+/****************************
+ * Getters
+ ****************************/
+export const getAboutEntry = async () => await getEntryBySlug("about", "about");
+
