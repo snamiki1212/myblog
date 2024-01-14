4e74074d233ffe4c36c99c2aac4021a079e93b4f
snamiki1212 <snamiki1212@gmail.com>
2024-01-14 17:38:56 +0900

    fix: update ui

--- src/constants/site.tsx
+++ src/constants/site.tsx
@@ -11,8 +11,6 @@ export const SITE_URL = "https://snamiki1212.com";
 
 export const COPYRIGHT = "© 2019-2023 Shun Namiki a.k.a Nash";
 
-export const SEARCH_ENGINE_ID = "374090a26f4208ea1";
-
 export const PAGE_SIZE = 20;
 export const LATEST_PAGE_SIZE = 4;
 
