abb307e978c740a437d5da7374d48ca88e75bf9d
snamiki1212 <26793088+snamiki1212@users.noreply.github.com>
2026-08-23 16:50:28 +0900

    chore: Tailwind CSS 4とdaisyUI 5へ更新

--- src/components/Container.WorkaroundViewTransition.tsx
+++ src/components/Container.WorkaroundViewTransition.tsx
@@ -11,5 +11,5 @@ type Props = {
 };
 
 export const Container = ({ children }: Props) => {
-  return <div className="max-w-screen-lg m-auto md:px-8">{children}</div>;
+  return <div className="max-w-(--breakpoint-lg) m-auto md:px-8">{children}</div>;
 };
